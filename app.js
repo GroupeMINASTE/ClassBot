4d5fac5fa127bb1afa4c83f2d6d1fa834ccaa4fd
Nathan Fallet <contact@nathanfallet.me>
2020-04-02 18:01:21 +0200

    Settings up a database

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-// require the discord.js module
+// require the discord.js and sqlite3 modules
 const Discord = require('discord.js');
+const sqlite3 = require('sqlite3').verbose();
 
 // create a new Discord client
 const client = new Discord.Client();
+
+// Create and connect to the database
+const db = new sqlite3.Database('./database.db', (err) => {
+  // Check for errors
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+
+  // No error
+  console.log('Connected to database!');
+});
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
@@ -12,12 +25,15 @@ client.once('ready', () => {
 
 // Listen for messages
 client.on('message', message => {
-  // Ping
-  if (message.content === '!ping') {
-  	// send back "Pong." to the channel the message was sent in
-  	message.channel.send('Pong.');
+  // Skip non commands
+  if (!message.content.startsWith('$') || message.author.bot) return;
+
+  // Get args and command
+  const args = message.content.slice(prefix.length).split(' ');
+  const command = args.shift().toLowerCase();
+
+  // Handle commands
+  if (command == 'ping') {
+    message.reply('Pong'):
   }
 });
-
-// login to Discord with your app's token
-client.login(process.env.TOKEN);
