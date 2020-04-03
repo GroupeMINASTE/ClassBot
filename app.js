c5908df548ef4ad36d966828f59a4ed0c4f346f2
Nathan Fallet <contact@nathanfallet.me>
2020-04-03 11:18:44 +0200

    Changing from SQLite to MySQL

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // require the discord.js and sqlite3 modules
 const Discord = require('discord.js');
-const sqlite3 = require('sqlite3').verbose();
+const mysql = require('mysql');
 const https = require('https');
 const express = require('express');
 
@@ -18,65 +18,74 @@ setInterval(() => {
 // create a new Discord client
 const client = new Discord.Client();
 
-// Create and connect to the database
-const db = new sqlite3.Database('./database.db', (err) => {
+// Connect to the database
+var con = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+});
+con.connect(function(err) {
   // Check for errors
-  if (err) {
-    console.error(err.message);
-    return;
-  }
+  if (err) throw err;
 
-  // No error
-  console.log('Connected to database!');
-});
+  // We are connected!
+  console.log('Connected!');
 
-// Setup database
-db.run('CREATE TABLE IF NOT EXISTS `profs` (`id` int(11) NOT NULL PRIMARY KEY, `user` bigint(11) NOT NULL, `name` varchar(255) NOT NULL);');
-db.run('CREATE TABLE IF NOT EXISTS `cours` (`id` int(11) NOT NULL PRIMARY KEY, `prof` int(11) NOT NULL, `start` datetime NOT NULL);');
-db.run('CREATE TABLE IF NOT EXISTS `devoirs` (`id` int(11) NOT NULL PRIMARY KEY, `prof` int(11) NOT NULL, `content` text NOT NULL, `due` datetime NOT NULL);');
+  // Setup database
+  con.query('CREATE TABLE IF NOT EXISTS `profs` (`id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, `user` bigint(11) NOT NULL, `name` varchar(255) NOT NULL);', function (err, result) {
+    if (err) throw err;
+  });
+  con.query('CREATE TABLE IF NOT EXISTS `cours` (`id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, `prof` int(11) NOT NULL, `start` datetime NOT NULL);', function (err, result) {
+    if (err) throw err;
+  });
+  con.query('CREATE TABLE IF NOT EXISTS `devoirs` (`id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, `prof` int(11) NOT NULL, `content` text NOT NULL, `due` datetime NOT NULL);', function (err, result) {
+    if (err) throw err;
+  });
 
-// when the client is ready, run this code
-// this event will only trigger one time after logging in
-client.once('ready', () => {
-	console.log('Ready!');
-});
+  // when the client is ready, run this code
+  // this event will only trigger one time after logging in
+  client.once('ready', () => {
+  	console.log('Ready!');
+  });
 
-// Listen for messages
-client.on('message', message => {
-  // Skip non commands
-  if (!message.content.startsWith('$') || message.author.bot) return;
+  // Listen for messages
+  client.on('message', message => {
+    // Skip non commands
+    if (!message.content.startsWith('$') || message.author.bot) return;
 
-  // Get args and command
-  const args = message.content.slice(1).split(/ +/);
-  const command = args.shift().toLowerCase();
+    // Get args and command
+    const args = message.content.slice(1).split(/ +/);
+    const command = args.shift().toLowerCase();
 
-  // Handle commands
-  if (command == 'ping') {
-    // Ping
-    message.reply('Pong');
-  } else if (command == 'prof') {
-    // Add a teacher
-    if (message.author.id == 238894740534198274) {
-      if (args.length == 2) {
-        message.reply('J\'ajoute ça tout de suite dans la base de données...');
+    // Handle commands
+    if (command == 'ping') {
+      // Ping
+      message.reply('Pong');
+    } else if (command == 'prof') {
+      // Add a teacher
+      if (message.author.id == 238894740534198274) {
+        if (args.length == 2) {
+          message.reply('J\'ajoute ça tout de suite dans la base de données...');
 
-        // Add to database
-        db.run(`INSERT INTO profs (user, name) VALUES(?, ?)`, args, function(err) {
-          if (err) {
-            return console.log(err.message);
-          }
+          // Add to database
+          con.query('INSERT INTO profs (user, name) VALUES(?, ?)', args, (err, results, fields) => {
+            if (err) {
+              return console.error(err.message);
+            }
 
-          // Confirme
-          message.channel.send('Parfait, <@' + args[0] + '> est maintenant défini(e) comme professeur(e) de ' + args[1])
-        });
+            // Confirme
+            message.channel.send('Parfait, <@' + args[0] + '> est maintenant défini(e) comme professeur(e) de ' + args[1])
+          });
+        } else {
+          message.reply('Il y a un problème avec ta commande, essaye $prof <id> <matière>');
+        }
       } else {
-        message.reply('Il y a un problème avec ta commande, essaye $prof <id> <matière>');
+        message.reply('Tu n\'as pas le droit de gérer la liste des professeurs, demande à <@238894740534198274> de le faire.');
       }
-    } else {
-      message.reply('Tu n\'as pas le droit de gérer la liste des professeurs, demande à <@238894740534198274> de le faire.');
     }
-  }
-});
+  });
 
-// login to Discord with your app's token
-client.login(process.env.TOKEN);
+  // login to Discord with your app's token
+  client.login(process.env.TOKEN);
+});
